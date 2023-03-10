f504239e46a227019eca17d7ae8137b8839b2c5c
nursal2404 <muhadzinur@gmail.com>
2023-03-10 23:36:21 +0700

    Authentification from Laravel

---
+++ resources/views/index.blade.php
@@ -0,0 +1,542 @@
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <!-- Basic -->
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <!-- Mobile Metas -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <!-- Site Metas -->
+  <link rel="icon" href="images/fevicon.png" type="image/gif" />
+  <meta name="keywords" content="" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+
+  <title>Tefa Enggano</title>
+
+
+  <!-- bootstrap core css -->
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+
+  <!-- fonts style -->
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet">
+
+  <!-- font awesome style -->
+  <link href="css/font-awesome.min.css" rel="stylesheet" />
+
+  <!-- Custom styles for this template -->
+  <link href="css/style.css" rel="stylesheet" />
+  <!-- responsive style -->
+  <link href="css/responsive.css" rel="stylesheet" />
+
+</head>
+
+<body>
+
+  <!-- header section strats -->
+  <header class="header_section">
+    <div class="container-fluid">
+      <nav class="navbar navbar-expand-lg custom_nav-container ">
+        <a class="navbar-brand" href="index.php">
+          <span>Bengkel Enggano</span>
+        </a>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class=""> </span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav  ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="index.php">Beranda <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="about.html"> Tentang Tefa</a>
+            </li>
+           <!--  <li class="nav-item">
+              <a class="nav-link" href="service.html">Service</a>
+            </li> -->
+            <li class="nav-item">
+              <a class="nav-link" href="why.html">Kenapa Memilih Kami</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="contact.html">Kontak Kami</a>
+            </li>
+          </ul>
+          <div class="quote_btn-container">
+            <form class="form-inline">
+              <button class="btn   nav_search-btn" type="submit">
+                <i class="fa fa-search" aria-hidden="true"></i>
+              </button>
+            </form>
+           <!--  <a href="">
+              <i class="fa fa-phone" aria-hidden="true"></i>
+              <span>
+                Call : +62 8127 5094 2323
+              </span>
+            </a> -->
+          </div>
+        </div>
+      </nav>
+    </div>
+  </header>
+  <!-- end header section -->
+  
+  <!-- slider section -->
+  <section class="slider_section ">
+    <div id="customCarousel1" class="carousel slide" data-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <div class="container ">
+            <div class="row">
+              <div class="col-md-6 col-lg-5">
+                <div class="detail-box">
+                  <h1>
+                    Enggano Motor<br>
+                    Service Motor
+                  </h1>
+                  <p>
+                    Selamat datang di TEFA SMKN 4 Kota Bengkulu, 
+                    Suatu Kerja nyata dari siswa siswi SMKN 4 Bengkulu untuk seluruh masyarakat bengkulu </p>
+                  <div class="btn-box">
+                    <a href="toko/shop.php" class="btn-1">
+                      Produk TEFA
+                    <a href="https://api.whatsapp.com/send?phone=+6282306325497&text=Halo%20CS%20Bengkel%20Enggano,%20mau%20Tanya%20Tanya%20Nih" class="btn-2">
+                      Hubungi Kami
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-6 col-lg-7">
+                <div class="img-box">
+                  <img src="images/slider-img.png" alt="">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <div class="container ">
+            <div class="row">
+              <div class="col-md-6 col-lg-5">
+                <div class="detail-box">
+                  <h1>
+                    Enggano Motor<br>
+                    Penjualan Suku Cadang
+                  </h1>
+                  <p>
+                    Selamat datang di TEFA SMKN 4 Kota Bengkulu, 
+                    Suatu Kerja nyata dari siswa siswi SMKN 4 Bengkulu untuk seluruh masyarakat bengkulu </p>
+                  <div class="btn-box">
+                    <a href="toko/shop.php" class="btn-1">
+                      Produk TEFA
+                    <a href="https://api.whatsapp.com/send?phone=+6282306325497&text=Halo%20CS%20Bengkel%20Enggano,%20mau%20Tanya%20Tanya%20Nih" class="btn-2">
+                      Hubungi Kami
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-6 col-lg-7">
+                <div class="img-box">
+                  <img src="images/slider-img.png" alt="">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <div class="container ">
+            <div class="row">
+              <div class="col-md-6 col-lg-5">
+                <div class="detail-box">
+                  <h1>
+                    Enggano Motor<br>
+                    Jual Beli Motor
+                  </h1>
+                  <p>
+                   Selamat datang di TEFA SMKN 4 Kota Bengkulu, 
+                   Suatu Kerja nyata dari siswa siswi SMKN 4 Bengkulu untuk seluruh masyarakat bengkulu </p>
+                  <div class="btn-box">
+                    <a href="toko/shop.php" class="btn-1">
+                      Produk TEFA
+                    <a href="https://api.whatsapp.com/send?phone=+6282306325497&text=Halo%20CS%20Bengkel%20Enggano,%20mau%20Tanya%20Tanya%20Nih" class="btn-2">
+                      Hubungi Kami
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-6 col-lg-7">
+                <div class="img-box">
+                  <img src="images/slider-img.png" alt="">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <ol class="carousel-indicators">
+        <li data-target="#customCarousel1" data-slide-to="0" class="active"></li>
+        <li data-target="#customCarousel1" data-slide-to="1"></li>
+        <li data-target="#customCarousel1" data-slide-to="2"></li>
+      </ol>
+    </div>
+  </section>
+  <!-- end slider section -->
+
+ <!-- service section -->
+  <section class="service_section ">
+    <div class="container">
+      <div class="heading_container heading_center">
+        <h2>
+          Produk Dan Jasa Kami
+        </h2>
+      </div>
+    </div>
+ <!-- service section -->
+
+    <!-- PEMELIHARAAN -->
+    <a href="sevice.php">
+    <div class="container ">
+      <div class="row">
+        <div class="col-md-6 col-lg-4">
+          <div class="box ">
+            <div class="img-box">
+              <img src="images/s1.png" alt="">
+            </div>
+            <div class="detail-box">
+              <h4>
+                Pemeliharaan 
+              </h4>
+              <p>
+                Jasa Pemeliharaan Motor, Jasa Perbaikan, Repaint Motor dan Tune Up
+              </p>
+              <a href="sevice.php">
+                <!-- Read More
+                <i class="fa fa-long-arrow-right" aria-hidden="true"></i> -->
+              </a>
+            </div>
+          </div>
+        </div>
+        </a>
+        <!-- PEMELIHARAAN -->
+
+        <!-- Jual Beli Motor-->
+        <a href="toko/shop.php">
+        <div class="col-md-6 col-lg-4">
+          <div class="box ">
+            <div class="img-box">
+              <img src="images/s2.png" alt="">
+            </div>
+            <div class="detail-box">
+              <h4>
+                Jual Beli Motor
+              </h4>
+              <p>
+                Yuk, Cek Motor Impian Kalian Di Toko Kami
+              </p>
+              <a href="toko/shop.php">
+                <!-- Read More
+                <i class="fa fa-long-arrow-right" aria-hidden="true"></i> -->
+              </a>
+            </div>
+          </div>
+        </div>
+        </a>
+        <!-- Jual Beli Motor-->
+
+        <!-- SUKU CADANG -->
+        <a href="toko/shop.php">
+        <div class="col-md-6 col-lg-4 ">
+          <div class="box ">
+            <div class="img-box">
+              <img src="images/s3.png" alt="">
+            </div>
+            <div class="detail-box">
+              <h4>
+               Jual Suku Cadang
+              </h4>
+              <p>
+                Suku Cadang Yang Pasti Asli Dari Produknya
+              </p>
+              <!-- <a href="">
+                Read More
+                <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+              </a> -->
+              </a>
+            </div>
+          </div>
+        </div>
+        <!-- SUKU CADANG -->
+
+       <!--  Produk Tefa -->
+       <a href="toko/shop.php">
+        <div class="col-md-6 col-lg-4">
+          <div class="box ">
+            <div class="img-box">
+              <img src="images/s4.png" alt="">
+            </div>
+            <div class="detail-box">
+              <h4>
+                Produk Tefa
+              </h4>
+              <p>
+               Yuk Cek Produk Yang Ditawrkan oleh Tefa SMKN 4 Bengkulu
+              </p>
+              <!-- <a href="">
+                Read More
+                <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+              </a> -->
+              </a>
+            </div>
+          </div>
+        </div>
+        <!--  Produk Tefa -->
+
+
+        <!-- Game Center -->
+        <a href="#">
+        <div class="col-md-6 col-lg-4">
+          <div class="box ">
+            <div class="img-box">
+              <img src="images/s5.png" alt="">
+            </div>
+            <div class="detail-box">
+              <h4>
+                Game Center
+              </h4>
+              <p>
+                Bosan? Yuk Cek Game Buatan Anak Anak Jurusan RPLv(Rekayasa Perangkat Lunak)
+              </p>
+              </a>
+            </div>
+          </div>
+        </div>
+         <!-- Game Center -->
+
+        <!-- Jasa Lainnya -->
+        <a href="https://api.whatsapp.com/send?phone=+6282306325497&text=Halo%20CS%20Tefa%20Enggano,%20Saya%20Konsultasi%20Tentang%20Sistem%20Dan Aplikasi">
+        <div class="col-md-6 col-lg-4">
+          <div class="box ">
+            <div class="img-box">
+              <img src="images/s6.png" alt="">
+            </div>
+            <div class="detail-box">
+              <h4>
+                Jasa Lainnya
+              </h4>
+              <p>
+                Mau buat Sistem/Aplikasi, Website penjualan, Pemerintahan, dan Sistem/Aplikasi Lainnya? Yuk Konsultasi Sama Kami
+              </p>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Jasa Lainnya -->
+    </div>
+  </section>
+  <!-- end service section -->
+
+   <!-- about section -->
+  <section class="about_section layout_padding">
+    <div class="heading_container heading_center">
+    <h2>
+        Tentang Kami
+    </h2>
+    </div>
+    <div class="container  ">
+      <div class="row">
+        <div class="col-md-6 ">
+          <div class="img-box">
+            <img src="images/slider-img.png" alt="">
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="detail-box">
+            
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has 
+              been the industry's standard dummy text ever since the 1500s, when an unknown printer took a 
+              galley of type and scrambled it to make a type specimen book. It has survived not 
+              only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+              It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+              and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </p>
+            <a href="">
+              Baca
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- end about section -->
+
+
+  <!-- why section -->
+  <section class="why_section layout_padding">
+    <div class="container">
+      <div class="heading_container heading_center">
+        <h2>
+          Kenapa Memilih Kami
+        </h2>
+      </div>
+    </div>
+    <div class="container ">
+      <div class="row">
+        <div class="col-md-6 col-lg-4 mx-auto">
+          <div class="box ">
+            <div class="img-box">
+              <img src="images/w1.png" alt="">
+            </div>
+            <div class="detail-box">
+              <h4>
+                Pekerja Profesional
+              </h4>
+              <p>
+               Pekerja Yang Memiliki Sertifikat Standar Nasional
+              </p>
+              <!-- <a href="">
+                Read More
+                <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+              </a> -->
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 col-lg-4 mx-auto ">
+          <div class="box ">
+            <div class="img-box">
+              <img src="images/w2.png" alt="">
+            </div>
+            <div class="detail-box">
+              <h4>
+                Suku Cadang Asli
+              </h4>
+              <p>
+                Produk Sparepart Dan Suku Cadang Asli  
+              </p>
+              <!-- <a href="">
+                Read More
+                <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+              </a> -->
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 col-lg-4 mx-auto">
+          <div class="box ">
+            <div class="img-box">
+              <img src="images/w3.png" alt="">
+            </div>
+            <div class="detail-box">
+              <h4>
+                Servis Terbaik
+              </h4>
+              <p>
+               Memberikan Servis Terbaik 
+              </p>
+              <!-- <a href="">
+                Read More
+                <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+              </a> -->
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- why section -->
+
+  <!-- info section -->
+  <br>
+  <br>
+  <br>
+  <section class="info_section ">
+    <div class="info_container ">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-9">
+            <div class="info_contact ">
+              <div class="row">
+                <div class="col-md-3">
+                  <a href="#" class="link-box">
+                    <i class="fa fa-map-marker" aria-hidden="true"></i>
+                    <span>
+                      Location
+                    </span>
+                  </a>
+                </div>
+                <div class="col-md-5">
+                  <a href="#" class="link-box">
+                    <i class="fa fa-phone" aria-hidden="true"></i>
+                    <span>
+                      Call +62 81274096XXX
+                    </span>
+                  </a>
+                </div>
+                <div class="col-md-4">
+                  <a href="#" class="link-box">
+                    <i class="fa fa-envelope" aria-hidden="true"></i>
+                    <span>
+                      info@tefaenggano.com
+                    </span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-5  col-lg-3 mx-auto">
+            <div class="info_form ">
+              <form action="#">
+                <input type="email" placeholder="Enter Your Email" />
+                <button>
+                  <i class="fa fa-arrow-right" aria-hidden="true"></i>
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div class="info_logo">
+          <a class="navbar-brand" href="index.html">
+            <span>
+              Bengkel Enggano
+            </span>
+          </a>
+        </div>
+        <div class="social-box">
+          <a href="">
+            <i class="fa fa-facebook" aria-hidden="true"></i>
+          </a>
+          <a href="">
+            <i class="fa fa-twitter" aria-hidden="true"></i>
+          </a>
+          <a href="">
+            <i class="fa fa-instagram" aria-hidden="true"></i>
+          </a>
+          <a href="">
+            <i class="fa fa-youtube" aria-hidden="true"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- end info section -->
+
+  <!-- footer section -->
+  <footer class="footer_section">
+    <div class="container">
+      <p>
+        &copy; <span id="displayYear"></span> TefaEnggano
+      </p>
+    </div>
+  </footer>
+  <!-- footer section -->
+
+  <!-- jQery -->
+  <script src="js/jquery-3.4.1.min.js"></script>
+  <!-- bootstrap js -->
+  <script src="js/bootstrap.js"></script>
+  <!-- custom js -->
+  <script src="js/custom.js"></script>
+</body>
+</html>
